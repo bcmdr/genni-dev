dda4f80244cd84b8de721b00eaf130f107c973cd
Brett Commandeur <bcommandeur@gmail.com>
2024-09-22 16:41:12 -0600

    finish ui of prototype

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   const [prompt, setPrompt] = useState<string>("");
   const [showIframe, setShowIframe] = useState<boolean>(false);
   const [inputPinned, setInputPinned] = useState<boolean>(false);
-  const [showEditor, setShowEditor] = useState<boolean>(true); // Show the editor during code generation
+  const [showEditor, setShowEditor] = useState<boolean>(false); // Show the editor during code generation
   const [iframeSrcDoc, setIframeSrcDoc] = useState<string>(""); // Manage iframe srcDoc
   const { completion, complete, isLoading } = useCompletion({
     api: "/api/completion",
@@ -56,25 +56,41 @@ export default function HomePage() {
       {/* Form for Input and Generate Button */}
       <form
         onSubmit={handleSubmit}
-        className={`w-full p-3 bg-white ${
+        className={`w-full bg-gray-300 ${
           inputPinned
-            ? "fixed top-0 z-50 shadow-md"
-            : "flex-grow flex items-center justify-center"
+            ? "sticky top-0 z-50 shadow-md"
+            : "flex-grow flex flex-col items-center justify-center p-10"
         }`}
       >
-        <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Enter your prompt"
-          className="w-3/4 p-3 text-lg border border-gray-300 rounded-md mr-4"
-        />
-        <button
-          type="submit"
-          className="px-5 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+        {!inputPinned && (
+          <div className="mb-7 flex text-2xl items-center justify-center">
+            <h3 className="font-bold mr-5 pr-5 border border-r-black border-t-transparent border-l-transparent border-b-transparent">
+              Genni
+            </h3>
+            <p>Idea to Prototype</p>
+          </div>
+        )}
+        <div
+          className={`flex flex-col sm:flex-row w-full  p-3 ${
+            inputPinned ? "bg-gray-200" : "bg-gray-300 max-w-3xl rounded"
+          }`}
         >
-          Generate
-        </button>
+          <input
+            type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Describe your idea..."
+            className={`px-3 py-1 text-lg rounded-md mb-4 sm:mb-0 sm:mr-4 bg-gray-900 text-white ${
+              inputPinned ? "flex-grow" : "w-full"
+            }`}
+          />
+          <button
+            type="submit"
+            className="px-5 py-2 text-white bg-gray-900 rounded-md hover:bg-gray-800"
+          >
+            Generate
+          </button>
+        </div>
       </form>
 
       {/* Show Code Editor during code generation */}
@@ -90,7 +106,7 @@ export default function HomePage() {
 
       {/* Show iframe after the code generation is completed */}
       {showIframe && (
-        <div className="flex-grow w-full h-full fixed top-0 bg-white">
+        <div className="flex-grow sticky w-full h-full fixed top-0 bg-white">
           <iframe
             className="w-full h-full"
             srcDoc={iframeSrcDoc}
@@ -100,14 +116,16 @@ export default function HomePage() {
       )}
 
       {/* Toggle button to show/hide the code editor or iframe */}
-      <div className="fixed bottom-0 left-0 w-full p-3 bg-white flex justify-between items-center shadow-md">
-        <button
-          onClick={handleToggleView}
-          className="px-5 py-2 text-white bg-gray-600 rounded-md hover:bg-gray-700"
-        >
-          {showEditor ? "Show Preview" : "Show Code"}
-        </button>
-      </div>
+      {completion && !isLoading && (
+        <div className="sticky w-full p-3 bg-white flex justify-between items-center shadow-md bg-gray-200">
+          <button
+            onClick={handleToggleView}
+            className="px-3 py-1 text-white bg-gray-950 rounded-md hover:bg-gray-800 cursor-pointer"
+          >
+            {showEditor ? "Show Preview" : "Show Code"}
+          </button>
+        </div>
+      )}
     </div>
   );
 }

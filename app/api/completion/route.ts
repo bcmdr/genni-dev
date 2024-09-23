53422dba4cdabba7e6b3db0867e2bb07da17bfb9
Brett Commandeur <bcommandeur@gmail.com>
2024-09-22 18:13:05 -0600

    Update prompt

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -5,9 +5,9 @@ export async function POST(req: Request) {
   const { prompt }: { prompt: string } = await req.json();
 
   const result = await streamText({
-    model: openai("gpt-4o-mini"),
+    model: openai("gpt-4o"),
     system:
-      "Ignore previous instruction. You are a web devlepment assistant. Only return html code snippets containing css and javascript. Interpret user questions as prompts for web page ideas. Do not introduce or explain the code, return only a code snippet with inline comments. No markdown.",
+      "Ignore previous instruction. You are a web devlepment assistant. Only return html code snippets containing css and javascript. Interpret user questions as prompts for web page ideas. Do not introduce or explain the code, return only a code snippet with inline comments. No markdown. Use modern box sizing and styling for the css to ensure high contrast accessible interfaces on mobile and desktop.",
     prompt,
   });
 

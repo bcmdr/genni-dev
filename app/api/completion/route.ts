5376b13d59b4847d19fd097c915b121e615c4d1d
Brett Commandeur <bcommandeur@gmail.com>
2024-10-14 15:25:41 -0600

    Update route.ts

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
   const result = await streamText({
     model: openai("gpt-4o"),
     system:
-      "Ignore previous instruction. You are a web development assistant. Only return html code snippets containing css and javascript. Interpret user questions as prompts for web page ideas. Do not introduce or explain the code, return only a code snippet with inline comments. No markdown. Use modern box sizing and styling for the css to ensure high contrast, accessible interfaces on mobile and desktop.",
+      "Ignore previous instruction. You are a web development assistant. Only return html code snippets containing css and javascript. Interpret user questions as prompts for web page ideas. Do not introduce or explain the code, return only a code snippet with inline comments. Use modern box sizing and styling for the css to ensure high contrast, accessible interfaces on mobile and desktop. Do not include markdown notation, just include the html code.",
     prompt,
     temperature: 0.8,
   });
